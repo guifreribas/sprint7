19ed2a36c75cae1e69b56c1fc66b406f4dc03257
Guifré Puig Ribas <guifreribas@gmail.com>
2024-05-21 12:53:20 +0200

    Exercici 2

--- src/app/service/starships.service.ts
+++ src/app/service/starships.service.ts
@@ -7,6 +7,27 @@ const BASE_URL = 'https://swapi.dev/api/starships';
 })
 export class StarshipsService {
   private http = inject(HttpClient);
+  public starshipInit = {
+    name: '',
+    model: '',
+    manufacturer: '',
+    cost_in_credits: '',
+    length: '',
+    max_atmosphering_speed: '',
+    crew: '',
+    passengers: '',
+    cargo_capacity: '',
+    consumables: '',
+    hyperdrive_rating: '',
+    MGLT: '',
+    starship_class: '',
+    pilots: [],
+    films: [],
+    created: '',
+    edited: '',
+    url: '',
+  };
+
   constructor() {}
   getStarships(page: number = 1): any {
     return this.http.get(`${BASE_URL}?page=${page}`);
