8fce6268bda83a997afc6b6b326ebc14c60d4fbb
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-22 17:21:16 +0200

    Add starship images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,40 @@ import { StarshipComponent } from '../starship/starship.component';
 export class HomeComponent implements OnInit {
   private starshipsService = inject(StarshipsService);
   public starshipsData: StarShip[] = [];
-  public starship = signal<StarShip>(this.starshipsService.starshipInit);
+  public starship = this.starshipsService.starship;
+  // public starship = signal<StarShip>(this.starshipsService.starshipInit);
   constructor() {}
 
-  selectStarship(starship: StarShip | null) {
-    this.starship.set(starship || this.starshipsService.starshipInit);
+  onSelectStarship(starship: StarShip | null): void {
+    if (!starship) return this.starship.set(this.starshipsService.starshipInit);
+    const oldStarship = starship;
+
+    if (oldStarship.name) {
+      let status: number;
+      const index = this.getStarshipIndex(oldStarship);
+      if (!index) return;
+      this.starshipsService.getStarshipImage(index).subscribe(
+        (data: any) => data,
+        (error: any) => {
+          if (error.status === 200) {
+            oldStarship.imgUrl = `https://starwars-visualguide.com/assets/img/starships/${index}.jpg`;
+          } else {
+            oldStarship.imgUrl =
+              'https://www.interactive.org/images/games_developers/no_image_available_sm.jpg';
+          }
+        }
+      );
+      oldStarship.index = index || '';
+      oldStarship.imgUrl = `https://starwars-visualguide.com/assets/img/starships/${index}.jpg`;
+      console.log(oldStarship.imgUrl);
+    }
+    this.starship.set(oldStarship);
+  }
+
+  getStarshipIndex(starship: StarShip): string | null {
+    const extractIdFromUrl = new RegExp(/\d+(?=\/$)/);
+    const match = starship.url.match(extractIdFromUrl);
+    return match ? match[0] : null;
   }
 
   ngOnInit(): void {
