2368758facef4b188144910511f5c441a438d1c1
Guifré Puig Ribas <gpuig@brothapps.com>
2024-05-30 18:29:36 +0200

    Exercici 9

---
+++ src/app/service/films.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Film, FilmData, StarShip } from '../models/interfaces';
+import { extractIndexFromUrl } from '../helpers/extractIndex';
+import { ConstantsService } from './constants.service';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmsService {
+  private http = inject(HttpClient);
+  private readonly constantsService = inject(ConstantsService);
+  public filmInit = {
+    title: '',
+    episode_id: 0,
+    opening_crawl: '',
+    director: '',
+    producer: '',
+    release_date: '',
+    characters: [],
+    planets: [],
+    starships: [],
+    vehicles: [],
+    species: [],
+    created: '',
+    edited: '',
+    url: '',
+  };
+  public film = signal<Film>(this.filmInit);
+
+  getFilm(index: string): any {
+    return this.http.get(`https://swapi.dev/api/films/${index}`);
+  }
+
+  async getFilmData(starship: StarShip): Promise<FilmData[]> {
+    let index = '';
+    let imgUrl = '';
+    let film: Film = this.filmInit;
+    const pilotsData: FilmData[] = [];
+    for (const filmUrl of starship.films) {
+      index = extractIndexFromUrl(filmUrl) || '';
+      imgUrl = !index
+        ? this.constantsService.IMG_FALLBACK_URL
+        : `${this.constantsService.IMG_FILMS_BASE_URL}${index}.jpg`;
+
+      if (index) {
+        try {
+          film = await lastValueFrom(this.getFilm(index));
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      pilotsData.push({
+        index,
+        imgUrl,
+        film,
+      });
+    }
+    return pilotsData;
+  }
+}
--- src/app/starship/starship.component.ts
+++ src/app/starship/starship.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, input } from '@angular/core';
 import { StarShip } from '../models/interfaces';
 import { PilotsComponent } from '../pilots/pilots.component';
+import { FilmsComponent } from '../films/films.component';
 
 @Component({
   selector: 'app-starship',
   standalone: true,
-  imports: [PilotsComponent],
+  imports: [PilotsComponent, FilmsComponent],
   templateUrl: './starship.component.html',
   styleUrl: './starship.component.scss',
 })
